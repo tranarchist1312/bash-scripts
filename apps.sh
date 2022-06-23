d63a1f761b608159eec3a805a56fcfea1cb19a95
tranarchist1312 <108103756+tranarchist1312@users.noreply.github.com>
2022-06-24 02:52:19 +0300

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# installs a basic suite of apps that I use, an emoji font, as well as git and base-devel which are needed for installing from the AUR
 sudo pacman -S git base-devel firefox discord flatpak gimp spectacle qbittorrent noto-fonts-emoji
 
 git clone https://aur.archlinux.org/yay.git
@@ -13,3 +14,4 @@ cd ~
 yay -S balena-etcher
 
 reboot
+# a reboot is required after installing flatpak
