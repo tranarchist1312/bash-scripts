da33ad626b010a3d5fd844e6ab0a2daaa7e4693e
tranarchist1312 <108103756+tranarchist1312@users.noreply.github.com>
2022-06-25 00:19:05 +0300

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+sudo pacman -Syu
+
 # installs a basic suite of apps that I use, an emoji font, as well as git and base-devel which are needed for installing from the AUR
-sudo pacman -S git base-devel firefox discord flatpak gimp spectacle qbittorrent noto-fonts-emoji
-
+ sudo pacman -S git base-devel firefox discord flatpak gimp spectacle qbittorrent noto-fonts-emoji neofetch libreoffice-still
+ 
+# installing the yay AUR helper
 git clone https://aur.archlinux.org/yay.git
-
 cd yay
-
 makepkg -si
 
 cd ~
